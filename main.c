013965058567661c7624b223396af9a45080610d
Akash Balasubramaniam <abalasub@bess-f2r2s17.clusters.42paris.fr>
2025-01-17 16:24:39 +0100

    replace Bresenham by DDA

--- main.c
+++ main.c
@@ -21,24 +21,34 @@ void	my_mlx_pixel_put(t_frame *data, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
-void	draw_line(int **map, t_frame frame, double x0, double y0, double x1, double y1)
+void draw_line(int **map, t_frame frame, double x0, double y0, double x1, double y1)
 {
-	double slope;
+	double dx;
+	double dy;
+	int step;
+	int i;
 	double x;
 	double y;
-	if (x0 > x1)
-		draw_line(map, frame, x1 ,y1, x0 , y0);
-	slope = (y1 - y0) / (x1 - x0);
-	x = x0;
-	while (x <= x1)
-	{
-		y = slope * (x - x0) + y0;
-		my_mlx_pixel_put(&frame, x, y, 0xFF0000);
-		(void) frame;
-		x++;
-	}
-}
 
+	dx = x1 - x0;
+	dy = y1 - y0;
+
+	x = x0;
+	y = y0;
+	i = 0;
+	step = (int)fmax(fabs(dx), fabs(dy));
+
+	while (i < step)
+	{
+		x += dx / step;
+		y += dy / step;
+		if (map[(int)y/50][(int)x/50])
+			return;
+		my_mlx_pixel_put(&frame, x, y, 0xFF0000);
+		i++;
+	}
+	(void)map;
+}
 void	initMap(t_vars *vars, int map[10][10])
 {
 	int i;
@@ -108,8 +118,8 @@ void displayFrame(t_player player, int **map, t_frame frame, t_vars vars)
 	
 	draw_map(map, 10, 0xFFFFFF, frame);
 	draw_player(player, frame);
+	// Desssine un rayon de ~100 pixels depuis le joueur 
 	draw_line(map, frame, player.pos_x, player.pos_y, player.pos_x + player.dir[0] * 100, player.pos_y + player.dir[1] * 100);
-	//draw_line(frame, (int)player.pos_x, (int)player.pos_y, (int)player.pos_x + 100, 0);
 	printf("dirX : %f dirY : %f\n", vars.player->dir[0], vars.player->dir[1]);
 	mlx_put_image_to_window(vars.mlx, vars.win, frame.img, 0, 0);
 }
@@ -136,6 +146,7 @@ int key_press(int keycode, t_vars *vars)
 	}
 	if (keycode == 115 && !checkCollision(vars, pos_x - vars->player->dir[0] * 5, pos_y - vars->player->dir[1] * 5))
 	{
+		// Le joueur se deplace de ~-5 pixels
 		vars->player->pos_x -= vars->player->dir[0] * 5;
 		vars->player->pos_y -= vars->player->dir[1] * 5;
 	}
@@ -143,6 +154,7 @@ int key_press(int keycode, t_vars *vars)
 	{
 		vars->player->angle = -0.1;
 		oldDir = vars->player->dir[0];
+		// Modifie la direction du vecteur de -0.1 en appliquant une formule
 		vars->player->dir[0] = (vars->player->dir[0] * cosf(vars->player->angle)) - (vars->player->dir[1] * sinf(vars->player->angle));
 		vars->player->dir[1] = (oldDir * sinf(vars->player->angle) + (vars->player->dir[1] * cosf(vars->player->angle)));
 	}
@@ -150,6 +162,7 @@ int key_press(int keycode, t_vars *vars)
 	{
 		vars->player->angle = 0.1;
 		oldDir = vars->player->dir[0];
+		// Modifie la direction du vecteur de -0.1 en appliquant une formule
 		vars->player->dir[0] = (vars->player->dir[0] * cosf(vars->player->angle)) - (vars->player->dir[1] * sinf(vars->player->angle));
 		vars->player->dir[1] = (oldDir * sinf(vars->player->angle) + (vars->player->dir[1] * cosf(vars->player->angle)));
 	}
