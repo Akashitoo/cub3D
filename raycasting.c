0b312d66c671d7a10d1da28b798eab5d9ad5717e
Akash Balasubramaniam <abalasub@paul-f4Ar5s5.clusters.42paris.fr>
2025-01-23 17:31:22 +0100

    fix fish eye

--- raycasting.c
+++ raycasting.c
@@ -18,8 +18,8 @@ void	init_ray(t_ray *ray, t_player player, double ray_x, double ray_y)
 	ray->ray_y = ray_y;
 	ray->map_x = (int)player.pos_x;
 	ray->map_y = (int)player.pos_y;
-	ray->delta_x = sqrt(1 + (ray_y * ray_y) / (ray_x * ray_x));
-	ray->delta_y = sqrt(1 + (ray_x * ray_x) / (ray_y * ray_y));
+	ray->delta_x =  fabs(1 / ray_x);//sqrt(1 + (ray_y * ray_y) / (ray_x * ray_x));
+	ray->delta_y = fabs(1 / ray_y);//sqrt(1 + (ray_x * ray_x) / (ray_y * ray_y));
 	if (ray_x < 0)
 	{
 		ray->step_x = -1;
